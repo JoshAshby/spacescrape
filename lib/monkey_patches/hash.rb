5fc1e935f427ac9fdfb7d73e33d3db7dc72038e4
JoshAshby <joshuaashby@joshashby.com>
2015-10-24 12:53:12 -0600

    add stringify_keys as well

--- lib/monkey_patches/hash.rb
+++ lib/monkey_patches/hash.rb
@@ -12,4 +12,18 @@ class Hash
       end
     end
   end
+
+  def stringify_keys
+    Hash[self.map{ |k, v| [k.to_s, v] }]
+  end
+
+  def deep_stringify_keys
+    self.reduce({}) do |memo, (k, v)|
+      if v.kind_of? Hash
+        memo.merge({ k.to_s => v.deep_stringify_keys })
+      else
+        memo.merge({ k.to_s => v })
+      end
+    end
+  end
 end
