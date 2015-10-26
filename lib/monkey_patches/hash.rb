f76423747dc0213ebfef8aa4e8fcb4a485a8ac9a
JoshAshby <joshuaashby@joshashby.com>
2015-10-26 00:55:14 -0600

    some code rewrite, think tests fail?

--- lib/monkey_patches/hash.rb
+++ lib/monkey_patches/hash.rb
@@ -1,10 +1,41 @@
+# Few monkey patches to support symbolizing and stringifying keys within a
+# hash. This is useful for example, if you want to use a hash as a splat'd
+# input to a function, where the keys of the hash must all be symbols.
+#
+# @note Most methods here are dumb and don't know how to handle things like
+#   calling to_sym on a regex or similar.
+#
+# Also provides a general #transform_keys() and #transform_values()
+#
+# Unlike ActiveSupport this doesn't contain any bang operators for these
+# methods.
 class Hash
+  def transform_keys
+    return self.enum_for :transform_keys unless block_given?
+
+    self.reduce(self.class.new) do |memo, (k, v)|
+      new_key = yield k
+      memo.merge({ new_key => v })
+    end
+  end
+
+  def transform_keys
+    return self.enum_for :transform_keys unless block_given?
+
+    self.reduce(self.class.new) do |memo, (k, v)|
+      new_val = yield v
+      memo.merge({ k => new_val })
+    end
+  end
+
   def symbolize_keys
-    Hash[self.map{ |k, v| [k.to_sym, v] }]
+    self.reduce(self.class.new) do |memo, (k, v)|
+      memo.merge({ k.to_sym => v })
+    end
   end
 
   def deep_symbolize_keys
-    self.reduce({}) do |memo, (k, v)|
+    self.reduce(self.class.new) do |memo, (k, v)|
       if v.kind_of? Hash
         memo.merge({ k.to_sym => v.deep_symbolize_keys })
       else
@@ -14,11 +45,13 @@ class Hash
   end
 
   def stringify_keys
-    Hash[self.map{ |k, v| [k.to_s, v] }]
+    self.reduce(self.class.new) do |memo, (k, v)|
+      memo.merge({ k.to_s => v })
+    end
   end
 
   def deep_stringify_keys
-    self.reduce({}) do |memo, (k, v)|
+    self.reduce(self.class.new) do |memo, (k, v)|
       if v.kind_of? Hash
         memo.merge({ k.to_s => v.deep_stringify_keys })
       else
--- test/lib/monkey_patches/hash_test.rb
+++ test/lib/monkey_patches/hash_test.rb
@@ -12,6 +12,9 @@ class HashTest < MiniTest::Test
       'piggy' => ['frog']
     }.symbolize_keys
 
+    ap expected
+    ap result
+
     assert result.keys.none?{ |k| k.kind_of? String }, "We have String keys in the resulting hash still!"
     assert expected == result, "Hashes with symbol keys don't match!"
   end
@@ -32,34 +35,84 @@ class HashTest < MiniTest::Test
     assert expected == result, "Hashes with symbol keys don't match!"
   end
 
+  def test_unconventional_symbolize_keys
+    obj = Object.new
+
+    assert_raises NoMethodError, "Should have ran into a problem while symbolizing keys" do
+      {
+        'kermit' => 'pig',
+        'piggy' => ['frog'],
+        /a/ => :a,
+        [ 1 ] => :a,
+        obj => :a,
+        1 => :a
+      }.symbolize_keys
+    end
+  end
+
+  def test_unconventional_deep_symbolize_keys
+    obj = Object.new
+
+    assert_raises NoMethodError, "Should have ran into a problem while symbolizing keys" do
+      {
+        'kermit' => 'pig',
+        'piggy' => { 'kermit' => 'frog' },
+        /a/ => :a,
+        [ 1 ] => :a,
+        obj => :a,
+        1 => :a
+      }.deep_symbolize_keys
+    end
+  end
+
   def test_stringify_keys
+    obj = Object.new
+
     expected = {
       'kermit' => 'pig',
-      'piggy' => ['frog']
+      'piggy' => ['frog'],
+      /a/ => :a,
+      [ 1 ] => :a,
+      obj => :a,
+      1 => :a
     }
 
     result = {
       kermit: 'pig',
-      piggy: ['frog']
+      piggy: ['frog'],
+      /a/ => :a,
+      [ 1 ] => :a,
+      obj => :a,
+      1 => :a
     }.stringify_keys
 
     assert result.keys.none?{ |k| k.kind_of? Symbol }, "We have Symbol keys in the resulting hash still!"
-    assert expected == result, "Hashes with symbol keys don't match!"
+    assert expected == result, "Hashes with string keys don't match!"
   end
 
   def test_deep_stringify_keys
+    obj = Object.new
+
     expected = {
       'kermit' => 'pig',
-      'piggy' => { 'kermit' => 'frog' }
+      'piggy' => { 'kermit' => 'frog' },
+      /a/ => :a,
+      [ 1 ] => :a,
+      obj => :a,
+      1 => :a
     }
 
     result = {
       kermit: 'pig',
-      piggy: { kermit: 'frog' }
+      piggy: { kermit: 'frog' },
+      /a/ => :a,
+      [ 1 ] => :a,
+      obj => :a,
+      1 => :a
     }.deep_stringify_keys
 
     assert result.keys.none?{ |k| k.kind_of? Symbol }, "We have Symbol keys in the resulting hash still!"
     assert result['piggy'].keys.none?{ |k| k.kind_of? Symbol }, "We have Symbol keys in the resulting hash still!"
-    assert expected == result, "Hashes with symbol keys don't match!"
+    assert expected == result, "Hashes with string keys don't match!"
   end
 end
