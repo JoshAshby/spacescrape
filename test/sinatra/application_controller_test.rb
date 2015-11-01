5ff8f726e8cc98714a4f84af19f965ff16b4dee4
JoshAshby <joshuaashby@joshashby.com>
2015-10-31 21:36:45 -0600

    hopefully fix tests

---
+++ test/sinatra/application_controller_test.rb
@@ -0,0 +1,14 @@
+require_relative '../test_helper'
+
+class ApplicationControllerTest < MiniTest::Test
+  include Rack::Test::Methods
+
+  def app
+    ApplicationController
+  end
+
+  def test_get_index
+    get '/'
+    assert last_response.ok?
+  end
+end
