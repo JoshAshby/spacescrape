11f3ea68f1ed4c8ce77826b7799a0a65362c391c
JoshAshby <joshuaashby@joshashby.com>
2015-11-09 17:00:29 -0700

    wip on working towards training the analyzer

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,4 +2,16 @@ class Topic < Sequel::Model
   one_to_many :keywords
   one_to_many :settings
   one_to_many :blacklists
+
+  def validate
+    super
+
+    errors.add :name, 'cannot be empty' if !name || name.empty?
+  end
+
+  def before_save
+    return false if super == false
+
+    self.key = self.name.downcase.gsub ' ', '_'
+  end
 end
--- app/workers/sneakers/analyze_worker.rb
+++ app/workers/sneakers/analyze_worker.rb
@@ -1,14 +1,19 @@
 module Workers
-  class ScrapeWorker
+  class AnalyzeWorker
     include Sneakers::Worker
     from_queue 'analyze'
 
     def work msg
       data = JSON.parse msg
-
       analyzer = Workflows::Analyze.new
 
-      analyzer.process url: data[:url]
+      analyzer.subscribe to: 'doc:fetched' do |bus, env|
+        env[:model].links.each do |link|
+          self.class.enqueue({ url: link }.to_json)
+        end
+      end
+
+      analyzer.process url: data['url']
 
       ack!
     end
